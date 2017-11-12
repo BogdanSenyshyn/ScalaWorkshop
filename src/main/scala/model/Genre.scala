36d50d5be98e79a0eefd4e43169c1d57c53ae607
Bogdan Senyshyn <bpsenyshyn@gmail.com>
2017-11-12 16:10:10 +0200

    Added foreign keys for extra tables

--- src/main/scala/model/Genre.scala
+++ src/main/scala/model/Genre.scala
@@ -4,10 +4,10 @@ import slick.jdbc.PostgresProfile.api._
 case class Genre (id: Option[Long], title: String, description: Option[String])
 
 class GenreTable(tag: Tag) extends Table[Genre](tag, "genre") {
-  val id = column[Option[Long]]("id", O.PrimaryKey)
+  val id = column[Long]("id", O.PrimaryKey)
   val title = column[String]("title")
   val description = column[Option[String]]("description")
 
-  def * = (id, title, description) <> (Genre.apply _ tupled, Genre.unapply) //this is better than an example, tupled - 2 paramet
+  def * = (id.?, title, description) <> (Genre.apply _ tupled, Genre.unapply) //this is better than an example, tupled - 2 paramet
   // ers - 1 parameter for previous tuple
 }
--- src/main/scala/model/Staff.scala
+++ src/main/scala/model/Staff.scala
@@ -4,11 +4,11 @@ import slick.jdbc.PostgresProfile.api._
 case class Staff (id: Option[Long], name: String, rate: Double, age: Int)
 
 class StaffTable(tag: Tag) extends Table[Staff](tag, "staff") {
-  val id = column[Option[Long]]("id", O.PrimaryKey)
+  val id = column[Long]("id", O.PrimaryKey)
   val name = column[String]("name")
   val rate = column[Double]("rate")
   val age = column[Int]("age")
 
-  def * = (id, name, rate, age) <> (Staff.apply _ tupled, Staff.unapply) //this is better than an example, tupled - 2 paramet
+  def * = (id.?, name, rate, age) <> (Staff.apply _ tupled, Staff.unapply) //this is better than an example, tupled - 2 paramet
   // ers - 1 parameter for previous tuple
 }
